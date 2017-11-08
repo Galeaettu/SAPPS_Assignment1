84e2d3425207712dad42cdc3508be3da71219dfb
Christian Galea <galeaettu@gmail.com>
2017-11-08 22:00:44 +0100

    UsersController, Register

---
+++ Assignment1/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLogic;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User u)
+        {
+            UsersOperations uo = new UsersOperations();
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    uo.Register(u);
+
+                    ViewData["message"] = "User registered successfully";
+                    ModelState.Clear();
+                }
+            }
+            catch(UsernameExistsException ex)
+            {
+                ModelState.AddModelError("Username", ex.Message);
+            }
+            catch(Exception ex)
+            {
+                // Add log to error
+                ViewData["message"] = "User registration failed";
+            }
+            return View();
+        }
+    }
+}
